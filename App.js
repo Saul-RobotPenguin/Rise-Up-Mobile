5e58e00c17c841e2971711c6137b32efd5ab46d0
Saul <scalleja12345@gmail.com>
2024-02-26 19:52:25 -0500

    Added buttons with images for the homepage along with header iamges and images on button

--- App.js
+++ App.js
@@ -8,12 +8,44 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './component/Header';
 
 
+//Views
+import HomeScreen from "./views/HomeScreen";
+import ShelterScreen from "./views/ShelterScreen";
+import FeedbackScreen from "./views/FeedbackScreen";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Header />
+      <Stack.Navigator
+        screenOptions={{
+          title: "Rise Up",
+          headerStyle: {
+            backgroundColor: "#DBD8B3",
+          },
+          headerTintColor: "#343434",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ title: "Rise Up" }}
+        />
+        <Stack.Screen
+          name="Shelter"
+          component={ShelterScreen}
+          options={{ title: "Shelters Near You" }}
+        />
+        <Stack.Screen
+          name="Feedback"
+          component={FeedbackScreen}
+          options={{ title: "Rise Up" }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- views/HomeScreen.jsx
+++ views/HomeScreen.jsx
@@ -1,15 +1,96 @@
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-export default function HomeScreen() {
+const image = {
+  uri: "https://images.unsplash.com/photo-1532960401447-7dd05bef20b0?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bmV3JTIweW9yayUyMGNpdHl8ZW58MHx8MHx8fDA%3D",
+};
+export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 16, fontWeight: "700" }}>
-        Welcome to the Home Screen
-      </Text>
-      {/* <Button
-        title={"Go to Shelter"}
-        onPress={() => navigation.navigate("Shelter")}
-      /> */}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={image} style={styles.headerImage} />
+        <Text style={styles.headerText}>Rise Up</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Shelter")}
+        >
+          <Image source={image} style={styles.buttonImage} />
+          <Text style={styles.buttonText}>Shelter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Shelter")}
+        >
+          <Image source={image} style={styles.buttonImage} />
+          <Text style={styles.buttonText}>Healthcare</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Shelter")}
+        >
+          <Image source={image} style={styles.buttonImage} />
+          <Text style={styles.buttonText}>FoodBanks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Feedback")}
+        >
+          <Image source={image} style={styles.buttonImage} />
+          <Text style={styles.buttonText}>Feedback</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 300,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    zIndex: 1,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    marginTop: 20,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#DBD8B3",
+    width: "48%", // Adjust width to occupy 25% of the container's width
+    aspectRatio: 1, // Ensure button maintains a square shape
+    padding: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: "center",
+    elevation: 3, // Add elevation for shadow effect
+  },
+  buttonText: {
+    color: "#343434",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonImage: {
+    width: 50,
+    height: 50,
+    marginBottom: 10,
+  },
+});
